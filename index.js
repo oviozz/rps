2adda76ee6cfcebc38456f1ed5878c453d3814ea
oviozz <42685801+oviozz@users.noreply.github.com>
2023-03-11 03:07:49 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
+
+
 const options = ['rock', 'paper', 'scissor']
 const game_options = document.querySelectorAll('.items');
 
 const your_score = document.querySelector('#your-score')
 const computer_score = document.querySelector('#computer-score')
+const you_choose = document.querySelector('#you-choose')
+const computer_choose = document.querySelector('#computer-choose')
 
 
 const game_description = document.querySelector('.game-label')
@@ -17,6 +21,9 @@ function computer_play(user_chosen){
     const computer_chosen = options[Math.floor(Math.random()* options.length)]
 
     const result = (options.indexOf(user_chosen) - options.indexOf(computer_chosen) + 3) % 3;
+
+    you_choose.textContent = user_chosen
+    computer_choose.textContent = computer_chosen
 
     if (result === 0) {
         game_description.innerText = "it's a tie!"
@@ -49,4 +56,6 @@ document.querySelector('#reset').addEventListener
     computer_score.textContent = computer
     game_description.innerText = "Tap any options!"
     game_description.style.cssText = "font-style: italic; color: rgba(21, 67, 96, 0.25); animation-name: floating; animation-duration: 3s; animation-iteration-count: infinite; animation-timing-function: ease-in-out;";
+    you_choose.textContent = ''
+    computer_choose.textContent = ''
 })
